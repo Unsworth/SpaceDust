742e56f478f40ef7facbe8427be1b41d13093e56
Daniel Adam <daniel@clearbridgemobile.com>
2014-10-06 11:23:23 -0400

    Pushing items

---
+++ Assets/Scripts/WeaponMine.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponMine : MonoBehaviour {
+
+	public float mineSpeed;
+	public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+		Physics2D.IgnoreLayerCollision (8, 9, true);
+		rigidbody2D.velocity = new Vector2 (mineSpeed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rigidbody2D.rotation -= rotationSpeed;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Rock")
+		{
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
