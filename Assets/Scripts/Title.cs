af62edf7210ded97fefad0cff3594eb153e51a7c
David Unsworth <davidr@clearbridgemobile.com>
2014-10-10 13:53:56 -0400

    Finished Title Screen

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -15,13 +15,11 @@ public class Title : MonoBehaviour {
 		startTime = Time.time;
 
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		FadeIn ();
 		Stamp ();
 	}
-
 
 	void FadeIn()
 	{
@@ -33,14 +31,15 @@ public class Title : MonoBehaviour {
 
 	void Stamp()
 	{
-		if (Time.time > startTime + 2.0f) {
-			prototypeLabel.transform.position = Vector3.Lerp (prototypeLabel.transform.position, new Vector3(prototypeLabel.transform.position.x, prototypeLabel.transform.position.y, 0), 0.2f);
-			prototypeLabel.transform.localScale = Vector3.Lerp (prototypeLabel.transform.localScale, new Vector3(3,3,1), 0.2f);
+				if (Time.time > startTime + 2.0f) {
+						prototypeLabel.transform.position = Vector3.Lerp (prototypeLabel.transform.position, new Vector3 (prototypeLabel.transform.position.x, prototypeLabel.transform.position.y, -0.1f), 0.2f);
+						prototypeLabel.transform.localScale = Vector3.Lerp (prototypeLabel.transform.localScale, new Vector3 (3, 3, 1), 0.2f);
 
-			if(SimpleGesture.instance.GetGesture () == InputState.Tap)
-			{
-				Application.LoadLevel("DanielTestBed");
-			}
+						if (SimpleGesture.instance.GetGesture ().InputState == InputState.Tap || Input.GetKeyDown (KeyCode.Space)) {			
+								{
+										Application.LoadLevel ("DanielTestBed");
+								}
+						}
 				}
-	}
+		}
 }
