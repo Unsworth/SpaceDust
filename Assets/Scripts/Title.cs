8901f53da67ea544ce0eee53d0b3ff7a2350ed9e
David Unsworth <davidr@clearbridgemobile.com>
2014-10-10 11:45:16 -0400

    Title Page Test01

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	SpriteRenderer renderer;
+	float alphaValue = 0.0f;
+	float startTime;
+	public GameObject prototypeLabel;
+
+	// Use this for initialization
+	void Start () {
+	
+		renderer = GetComponent<SpriteRenderer> ();
+		startTime = Time.time;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FadeIn ();
+		Stamp ();
+	}
+
+
+	void FadeIn()
+	{
+		if (alphaValue < 1) {
+						renderer.color = new Color (1, 1, 1, alphaValue);
+						alphaValue += Time.deltaTime;
+				}
+		}
+
+	void Stamp()
+	{
+		if (Time.time > startTime + 2.0f) {
+			prototypeLabel.transform.position = Vector3.Lerp (prototypeLabel.transform.position, new Vector3(prototypeLabel.transform.position.x, prototypeLabel.transform.position.y, 0), 0.2f);
+			prototypeLabel.transform.localScale = Vector3.Lerp (prototypeLabel.transform.localScale, new Vector3(3,3,1), 0.2f);
+
+			if(SimpleGesture.instance.GetGesture () == InputState.Tap)
+			{
+				Application.LoadLevel("DanielTestBed");
+			}
+				}
+	}
+}
